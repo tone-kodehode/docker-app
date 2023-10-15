bffa37fa0a3b1341bff9635bafdab5bbec5d36d1
tone-kodehode <tone.kodehode@gmail.com>
2023-10-15 19:34:39 +0200

    adding comments to code

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
-const dotenv = require('dotenv'); 
-let app = express();
+// Import required packages
+let express = require('express'); // Express framework
+let path = require('path'); // Path module for handling file paths
+let fs = require('fs'); // File system module for file operations
+let MongoClient = require('mongodb').MongoClient; // MongoDB client
+let bodyParser = require('body-parser'); // Middleware for parsing request data
+const cors = require('cors'); // CORS middleware for enabling cross-origin requests
+const dotenv = require('dotenv'); // Environment variable configuration
+let app = express(); // Create an Express application
 
+// Middleware 
+// Use CORS middleware to enable cross-origin requests
+app.use(cors());
 
+// Use body-parser middleware to parse request data
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json();
 
+// Serve the HTML file at the root URL '/'
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
+// Serve profile picture as an image
 app.get('/profile-picture', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.png"));
   res.writeHead(200, {'Content-Type': 'image/png' });
   res.end(img, 'binary');
 });
 
-// starting application locally
+// Set MongoDB connection URLs for local and Docker environments
 const mongoUrlLocal = process.env.MONGO_URL_LOCAL;
-
-// starting application as docker container
 const mongoUrlDocker = process.env.MONGO_URL_DOCKER;
 
-// pass options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+// Configure MongoDB client options to avoid DeprecationWarning
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
-// "user-account" demo with docker. "docker-app" with docker-compose
+// Define the database name
 let databaseName = "docker-app";
 
+// Update user profile data
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
+  // Connect to the local MongoDB
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
@@ -46,7 +54,8 @@ app.post('/update-profile', function (req, res) {
     let myquery = { userid: 1 };
     let newvalues = { $set: userObj };
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+    // Update the user's profile data
+    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, result) {
       if (err) throw err;
       client.close();
     });
@@ -56,9 +65,11 @@ app.post('/update-profile', function (req, res) {
   res.send(userObj);
 });
 
+// Retrieve user profile data
 app.get('/get-profile', function (req, res) {
   let response = {};
-  // Connect to the db
+  
+  // Connect to the local MongoDB
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
@@ -66,6 +77,7 @@ app.get('/get-profile', function (req, res) {
 
     let myquery = { userid: 1 };
 
+    // Retrieve the user's profile data
     db.collection("users").findOne(myquery, function (err, result) {
       if (err) throw err;
       response = result;
@@ -77,6 +89,8 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
+// Start the Express server on port 3000
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
+
